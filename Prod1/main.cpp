51dc850b97558803ec1283d3768fc759637606d3
Shane Anderson <shane@pixelboogie.com>
2019-08-02 10:00:01 -0700

    This program counts the number of spaces in a string and outputs the total.

--- Prod1/main.cpp
+++ Prod1/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+
+void CountWords(std::string myString);
+string myString = "Hi, this is an example scentence with several spaces.\n";
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    cout << "Hello, this program counts the number of spaces in a string and outputs the total.\n";
+    CountWords(myString);
+    
     return 0;
 }
+
+
+void CountWords(std::string myString) {
+    int numspaces = 0;
+    char nextChar;
+    // checks each character in the string
+    for (int i=0; i<int(myString.length()); i++)
+    {
+        nextChar = myString.at(i); // gets a character
+        if (isspace(myString[i]))
+            numspaces++;
+    }
+    numspaces--; // decrement 1 because of the new line character
+    cout << "\nThere are " << numspaces << " spaces in this scentence.\n\n";
+    
+}
+
+
+
