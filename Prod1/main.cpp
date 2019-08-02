6a3706428dcfc21068ca5c48477ec67686f70b02
Shane Anderson <shane@pixelboogie.com>
2019-08-02 09:14:17 -0700

    Initial Commit

---
+++ Prod1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prod1
+//
+//  Created by Shane Anderson on 8/2/19.
+//  Copyright © 2019 Shane Anderson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
